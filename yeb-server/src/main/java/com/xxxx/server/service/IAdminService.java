c345c6cb359c19d35eabf9516780022fb6efda04
phil <xiefei_phil@126.com>
2022-11-12 18:13:06 +0800

    login part

---
+++ yeb-server/src/main/java/com/xxxx/server/service/IAdminService.java
@@ -0,0 +1,26 @@
+package com.xxxx.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.server.pojo.Admin;
+import com.xxxx.server.pojo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhoubin
+ * @since 2022-11-09
+ */
+public interface IAdminService extends IService<Admin> {
+    /**
+     * 登录之后返回token
+     * @param username
+     * @param password
+     * @param request
+     * @return
+     */
+    RespBean login(String username, String password, HttpServletRequest request);
+}
---
+++ yeb-server/src/main/java/com/xxxx/server/service/IMenuRoleService.java
@@ -0,0 +1,16 @@
+package com.xxxx.server.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.xxxx.server.pojo.MenuRole;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author zhoubin
+ * @since 2022-11-09
+ */
+public interface IMenuRoleService extends IService<MenuRole> {
+
+}
